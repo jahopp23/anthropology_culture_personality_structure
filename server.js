62be1d93757e95fec94598c5596df2b40570982a
John A. Hopper <jahopp@gmail.com>
2018-05-12 17:59:31 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/'));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+}); 
+
+app.listen(process.env.PORt || 8000);
+console.log("App live on Port:8000");
+
+
